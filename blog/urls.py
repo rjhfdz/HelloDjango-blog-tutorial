e96e88dc02b0483fbf5b65e5bc0ef4f7ffe5e892
rjhfdz <rjh_fdz@163.com>
2020-04-10 19:13:58 +0800

    项目上传

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/<int:pk>', views.detail, name='detail'),
+]
