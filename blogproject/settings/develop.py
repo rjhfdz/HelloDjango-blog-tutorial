c5a6dc687e0bcde8f77925f12b3aaefbba022114
rjhfdz <rjh_fdz@163.com>
2020-04-18 10:59:04 +0800

    分类统计功能 rss订阅 视图函数更改为类视图 添加分页功能

--- blogproject/settings/develop.py
+++ blogproject/settings/develop.py
@@ -10,3 +10,21 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
